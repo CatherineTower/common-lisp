c6b3f751926e4e6b8429e310dbb34f44bb423a89
Michael Fiano <mail@mfiano.net>
2022-01-12 19:59:57 -0500

    Add non-consing converters.

--- graphics/tools/color/src/convert.lisp
+++ graphics/tools/color/src/convert.lisp
@@ -1,76 +1,132 @@
 (in-package #:mfiano.graphics.tools.color)
 
-(defgeneric convert (color type)
-  (:method :around ((color color) type)
+(defgeneric convert (source target)
+  (:method :around ((source color) target)
     ;; Skip the conversion if the color is already of the given type.
-    (if (eq (class-name (class-of color)) type)
-        color
+    (if (eq (class-name (class-of source)) target)
+        source
         (call-next-method))))
 
-(defmethod convert ((color color) (type (eql 'rgba)))
+(defmethod convert ((source color) (target rgba))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
+  (u:mvlet ((r g b a (decompose source)))
     (declare (u:ub16 r g b a))
     (cond
       ((zerop a)
-       (rgba 0 0 0 0))
+       (setf (rgba-r target) 0
+             (rgba-g target) 0
+             (rgba-b target) 0
+             (rgba-a target) 0))
       ((= a #xffff)
-       (rgba (ash r -8) (ash g -8) (ash b -8) #xff))
+       (setf (rgba-r target) (ash r -8)
+             (rgba-g target) (ash g -8)
+             (rgba-b target) (ash b -8)
+             (rgba-a target) #xff))
       (t
-       (rgba (ash (truncate (- (ash r 16) r) a) -8)
-             (ash (truncate (- (ash g 16) g) a) -8)
-             (ash (truncate (- (ash b 16) b) a) -8)
-             (ash a -8))))))
+       (setf (rgba-r target) (truncate (- (ash r 16) r) a)
+             (rgba-g target) (truncate (- (ash g 16) g) a)
+             (rgba-b target) (truncate (- (ash b 16) b) a)
+             (rgba-a target) (ash a -8))))
+    target))
 
-(defmethod convert ((color color) (type (eql 'rgba-pma)))
+(defmethod convert ((source color) (target (eql 'rgba)))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
+  (convert source (rgba 0 0 0 0)))
+
+(defmethod convert ((source color) (target rgba-pma))
+  (declare (optimize speed))
+  (u:mvlet ((r g b a (decompose source)))
     (declare (u:ub16 r g b a))
-    (rgba-pma (ash r -8) (ash g -8) (ash b -8) (ash a -8))))
+    (setf (rgba-r target) (ash r -8)
+          (rgba-g target) (ash g -8)
+          (rgba-b target) (ash b -8)
+          (rgba-a target) (ash a -8))
+    target))
 
-(defmethod convert ((color color) (type (eql 'rgba16)))
+(defmethod convert ((source color) (target (eql 'rgba-pma)))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
+  (convert source (rgba-pma 0 0 0 0)))
+
+(defmethod convert ((source color) (target rgba16))
+  (declare (optimize speed))
+  (u:mvlet ((r g b a (decompose source)))
     (declare (u:ub16 r g b a))
     (cond
       ((zerop a)
-       (rgba16 0 0 0 0))
+       (setf (rgba16-r target) 0
+             (rgba16-g target) 0
+             (rgba16-b target) 0
+             (rgba16-a target) 0))
       ((= a #xffff)
-       (rgba16 (ash r -8) (ash g -8) (ash b -8) #xff))
+       (setf (rgba16-r target) (ash r -8)
+             (rgba16-g target) (ash g -8)
+             (rgba16-b target) (ash b -8)
+             (rgba16-a target) a))
       (t
-       (rgba16 (truncate (- (ash r 16) r) a)
-               (truncate (- (ash g 16) g) a)
-               (truncate (- (ash b 16) b) a)
-               a)))))
+       (setf (rgba16-r target) (truncate (- (ash r 16) r) a)
+             (rgba16-g target) (truncate (- (ash g 16) g) a)
+             (rgba16-b target) (truncate (- (ash b 16) b) a)
+             (rgba16-a target) a)))
+    target))
 
-(defmethod convert ((color color) (type (eql 'rgba16-pma)))
+(defmethod convert ((source color) (target (eql 'rgba16)))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
-    (declare (u:ub16 r g b a))
-    (rgba16-pma r g b a)))
+  (convert source (rgba16 0 0 0 0)))
 
-(defmethod convert ((color color) (type (eql 'alpha)))
+(defmethod convert ((source color) (target rgba16-pma))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
+  (u:mvlet ((r g b a (decompose source)))
+    (setf (rgba16-r target) r
+          (rgba16-g target) g
+          (rgba16-b target) b
+          (rgba16-a target) a)
+    target))
+
+(defmethod convert ((source color) (target (eql 'rgba16-pma)))
+  (declare (optimize speed))
+  (convert source (rgba16-pma 0 0 0 0)))
+
+(defmethod convert ((source color) (target alpha))
+  (declare (optimize speed))
+  (u:mvlet ((_ _ _ a (decompose source)))
     (declare (u:ub16 a))
-    (alpha (ash a -8))))
+    (setf (alpha-value target) (ash a -8))
+    target))
 
-(defmethod convert ((color color) (type (eql 'alpha16)))
+(defmethod convert ((source color) (target (eql 'alpha)))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
-    (declare (u:ub16 a))
-    (alpha16 a)))
+  (convert source (alpha 0)))
 
-(defmethod convert ((color color) (type (eql 'gray)))
+(defmethod convert ((source color) (target alpha16))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
-    (declare (u:ub16 r g b a))
+  (u:mvlet ((_ _ _ a (decompose source)))
+    (setf (alpha16-value target) a)
+    target))
+
+(defmethod convert ((source color) (target (eql 'alpha16)))
+  (declare (optimize speed))
+  (convert source (alpha16 0)))
+
+(defmethod convert ((source color) (target gray))
+  (declare (optimize speed))
+  (u:mvlet ((r g b (decompose source)))
+    (declare (u:ub16 r g b))
     ;; Uses the ITU-R Recommendation BT.709 standard for its luma coefficients.
-    (gray (ash (+ (* 13933 r) (* 46871 g) (* 4732 b) #x8000) -24))))
+    (setf (gray-value target) (ash (+ (* 13933 r) (* 46871 g) (* 4732 b) #x8000) -24))
+    target))
 
-(defmethod convert ((color color) (type (eql 'gray16)))
+(defmethod convert ((source color) (target (eql 'gray)))
   (declare (optimize speed))
-  (u:mvlet ((r g b a (decompose color)))
-    (declare (u:ub16 r g b a))
+  (convert source (gray 0)))
+
+(defmethod convert ((source color) (target gray16))
+  (declare (optimize speed))
+  (u:mvlet ((r g b (decompose source)))
+    (declare (u:ub16 r g b))
     ;; Uses the ITU-R Recommendation BT.709 standard for its luma coefficients.
-    (gray16 (ash (+ (* 13933 r) (* 46871 g) (* 4732 b) #x8000) -16))))
+    (setf (gray16-value target) (ash (+ (* 13933 r) (* 46871 g) (* 4732 b) #x8000) -16))
+    target))
+
+(defmethod convert ((source color) (target (eql 'gray16)))
+  (declare (optimize speed))
+  (convert source (gray16 0)))
