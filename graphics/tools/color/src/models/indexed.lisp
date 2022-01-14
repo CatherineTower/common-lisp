47d34db17e6947ef32761ae90ab914375b4a530e
Michael Fiano <mail@mfiano.net>
2022-01-14 12:23:38 -0500

    Fix indexed color conversion?

--- graphics/tools/color/src/models/indexed.lisp
+++ graphics/tools/color/src/models/indexed.lisp
@@ -2,19 +2,24 @@
 
 (defstruct (indexed
             (:include model)
-            (:constructor indexed (palette))
+            (:constructor %indexed)
             (:predicate nil)
             (:copier nil))
   (palette nil :type palette))
 
-(defmethod convert ((source model) (target palette))
-  (loop :with palette-colors := (palette-colors palette)
+(defun indexed (palette-name)
+  (%indexed :palette (make-palette palette-name)))
+
+(defmethod convert ((source model) (target indexed))
+  (loop :with palette-colors := (palette-colors (indexed-palette target))
         :with best := 0
+        :with best-index := 0
         :for palette-color :across palette-colors
         :for i :from 0
-        :for length-squared := (%length-squared color palette-color)
+        :for length-squared := (%length-squared source palette-color)
         :when (< length-squared best)
           :do (when (zerop length-squared)
                 (return (aref palette-colors i)))
-              (setf best length-squared)
-        :finally (return (aref palette-colors i))))
+              (setf best length-squared
+                    best-index i)
+        :finally (return (aref palette-colors best-index))))
