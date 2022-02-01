3686db4a55521e7efb99361e0d5104b6e9cfbfa6
Michael Fiano <mail@mfiano.net>
2022-02-01 17:47:04 -0500

    Generate normalized white point methods from chromaticity coordinates of standard illuminants to reduce source code and maintenance cost.

--- graphics/tools/image/src/color/white-point.lisp
+++ graphics/tools/image/src/color/white-point.lisp
@@ -1,125 +1,38 @@
 (in-package #:mfiano.graphics.tools.image)
 
+(defvar *standard-illuminant-chromaticity-coordinates*
+  '((:a (0.44757 0.40745))
+    (:b (0.34842 0.35161))
+    (:c (0.31006 0.31616))
+    (:d50 (0.34567 0.3585))
+    (:d55 (0.33242 0.34743))
+    (:d65 (0.31271 0.32902))
+    (:d75 (0.29902 0.31485))
+    (:d93 (0.28315 0.29711))
+    (:e (1/3 1/3))
+    (:f1 (0.3131 0.33727))
+    (:f2 (0.37208 0.37529))
+    (:f3 (0.4091 0.3943))
+    (:f4 (0.44018 0.40329))
+    (:f5 (0.31379 0.34531))
+    (:f6 (0.3779 0.38835))
+    (:f7 (0.31292 0.32933))
+    (:f8 (0.34588 0.35875))
+    (:f9 (0.37417 0.37281))
+    (:f10 (0.34609 0.35986))
+    (:f11 (0.38052 0.37713))
+    (:f12 (0.43695 0.40441))
+    (:led-b1 (0.4560 0.4078))
+    (:led-b2 (0.4357 0.4012))
+    (:led-b3 (0.3756 0.3723))))
+
 ;; Get a vector in XYZ space representing the white point of the given standard illuminant,
 ;; normalized with a luminance (Y) value of 1 and a standard observer field of view of 2 degrees.
 (defgeneric get-white-point (standard-illuminant))
 
-;; incandescent / tungsten
-(defmethod get-white-point ((standard-illuminant (eql :a)))
-  (load-time-value (v3:vec 1.098466 1 0.35582292)))
-
-;; obsolete, direct sunlight at noon
-(defmethod get-white-point ((standard-illuminant (eql :b)))
-  (load-time-value (v3:vec 0.9909274 1 0.85313267)))
-
-;; obsolete, average / North sky daylight
-(defmethod get-white-point ((standard-illuminant (eql :c)))
-  (load-time-value (v3:vec 0.980706 1 1.1822494)))
-
-;; horizon light, ICC profile PCS
-(defmethod get-white-point ((standard-illuminant (eql :d50)))
-  (load-time-value (v3:vec 0.964212 1 0.8251883)))
-
-;; mid-morning / mid-afternoon daylight
-(defmethod get-white-point ((standard-illuminant (eql :d55)))
-  (load-time-value (v3:vec 0.95679706 1 0.92148066)))
-
-;; noon daylight: television, sRGB color space
-(defmethod get-white-point ((standard-illuminant (eql :d65)))
-  (load-time-value (v3:vec 0.95042855 1 1.0889004)))
-
-;; North sky daylight
-(defmethod get-white-point ((standard-illuminant (eql :d75)))
-  (load-time-value (v3:vec 0.94972205 1 1.2263936)))
-
-;; high-efficiency blue phosphor monitors, BT.2035
-(defmethod get-white-point ((standard-illuminant (eql :d93)))
-  (load-time-value (v3:vec 0.953014 1 1.412743)))
-
-;; equal energy
-(defmethod get-white-point ((standard-illuminant (eql :e)))
-  (load-time-value (v3:vec 1 1 1)))
-
-;; daylight fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f1)))
-  (load-time-value (v3:vec 0.9283364 1 1.0366473)))
-
-;; cool white fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f2)))
-  (load-time-value (v3:vec 0.9914466 1 0.6731595)))
-
-;; white fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f3)))
-  (load-time-value (v3:vec 1.0375348 1 0.49860507)))
-
-;; warm white fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f4)))
-  (load-time-value (v3:vec 1.0914726 1 0.3881326)))
-
-;; daylight fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f5)))
-  (load-time-value (v3:vec 0.90871966 1 0.98722893)))
-
-;; light white fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f6)))
-  (load-time-value (v3:vec 0.97309124 1 0.60190547)))
-
-;; D65 simulator, daylight simulator
-(defmethod get-white-point ((standard-illuminant (eql :f7)))
-  (load-time-value (v3:vec 0.9501716 1 1.0862966)))
-
-;; D50 simulator, Sylvania F40 Design 50
-(defmethod get-white-point ((standard-illuminant (eql :f8)))
-  (load-time-value (v3:vec 0.96412545 1 0.823331)))
-
-;; cool white deluxe fluorescent
-(defmethod get-white-point ((standard-illuminant (eql :f9)))
-  (load-time-value (v3:vec 1.0036479 1 0.67868346)))
-
-;; Philips TL85, Ultralume 50
-(defmethod get-white-point ((standard-illuminant (eql :f10)))
-  (load-time-value (v3:vec 0.96173507 1 0.8171234)))
-
-;; Philips TL84, Ultralume 40
-(defmethod get-white-point ((standard-illuminant (eql :f11)))
-  (load-time-value (v3:vec 1.0089889 1 0.6426166)))
-
-;; Philips TL83, Ultralume 30
-(defmethod get-white-point ((standard-illuminant (eql :f12)))
-  (load-time-value (v3:vec 1.0804629 1 0.3922752)))
-
-;; phosphor-converted blue
-(defmethod get-white-point ((standard-illuminant (eql :led-b1)))
-  (load-time-value (v3:vec 1.1181952 1 0.33398736)))
-
-;; phosphor-converted blue
-(defmethod get-white-point ((standard-illuminant (eql :led-b2)))
-  (load-time-value (v3:vec 1.085992 1 0.4065304)))
-
-;; phosphor-converted blue
-(defmethod get-white-point ((standard-illuminant (eql :led-b3)))
-  (load-time-value (v3:vec 1.0088638 1 0.67714214)))
-
-;; phosphor-converted blue
-(defmethod get-white-point ((standard-illuminant (eql :led-b4)))
-  (load-time-value (v3:vec 0.9771559 1 0.87835515)))
-
-;; phosphor-converted blue
-(defmethod get-white-point ((standard-illuminant (eql :led-b5)))
-  (load-time-value (v3:vec 0.96353525 1 1.1266997)))
-
-;; mixing of phosphor-converted blue LED and red LED (blue-hybrid)
-(defmethod get-white-point ((standard-illuminant (eql :led-bh1)))
-  (load-time-value (v3:vec 1.1003443 1 0.35907534)))
-
-;; mixing of red, green, and blue LEDs
-(defmethod get-white-point ((standard-illuminant (eql :led-rgb1)))
-  (load-time-value (v3:vec 1.0821658 1 0.292567)))
-
-;; phosphor-converted violet
-(defmethod get-white-point ((standard-illuminant (eql :led-v1)))
-  (load-time-value (v3:vec 1.0026385 1 0.1961302)))
-
-;; phosphor-converted violet
-(defmethod get-white-point ((standard-illuminant (eql :led-v2)))
-  (load-time-value (v3:vec 1.0015894 1 0.6474171)))
+(macrolet ((generate-white-point-methods ()
+             `(progn
+                ,@(loop :for (name (x y)) :in *standard-illuminant-chromaticity-coordinates*
+                        :collect `(defmethod get-white-point ((standard-illuminant (eql ,name)))
+                                    (load-time-value (v3:vec ,(/ x y) 1 ,(/ (- 1 x y) y))))))))
+  (generate-white-point-methods))
