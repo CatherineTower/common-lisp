43fd6ad699ef18c4d1e198ab154b72a66266434f
Michael Fiano <mail@mfiano.net>
2022-02-03 04:17:57 -0500

    Add color space constructors

--- graphics/tools/image/src/color/spaces.lisp
+++ graphics/tools/image/src/color/spaces.lisp
@@ -83,8 +83,23 @@
   (:default-initargs
    :standard-illuminant :d50))
 
+(defun rgb (r g b &key (working-space 'srgb) standard-illuminant)
+  (check-type working-space rgb-working-space)
+  (let ((color (%make-color-space working-space standard-illuminant)))
+    (add-mixin color 'color-storage3 :c0 r :c1 g :c2 b)))
+
+(defun rgba (r g b a &key (working-space 'srgb) standard-illuminant pre-multiply-alpha)
+  (check-type working-space rgb-working-space)
+  (let ((color (%make-color-space working-space standard-illuminant)))
+    (add-mixin color 'color-storage4 :c0 r :c1 g :c2 b :c3 a)
+    (add-mixin color 'alpha :alpha-index 3 :pre-multiply-alpha pre-multiply-alpha)))
+
 ;;; XYZ
 
 (defclass xyz (color-space) ()
   (:default-initargs
    :standard-illuminant :e))
+
+(defun xyz (x y z &key standard-illuminant)
+  (let ((color (%make-color-space 'xyz standard-illuminant)))
+    (add-mixin color 'color-storage3 :c0 x :c1 y :c2 z)))
