48638753fa26d9639675df8ca16af20a4ed40e6a
Catherine Tower <catherine@towernet.dev>
2022-02-04 10:43:57 -0800

    Add XYZ to SRGB function
    It round trips!! This is a huge success!!!
    
    I'm thinking there's some changes that are going to have to be made. Consider
    this a temporary commit

--- graphics/tools/image/src/color/spaces/conversion.lisp
+++ graphics/tools/image/src/color/spaces/conversion.lisp
@@ -5,21 +5,6 @@
 ;;;; NOTE: This file must be loaded after all color space types are defined because the method
 ;;;; specializers must reference defined classes.
 
-;;; XYZ to sRGB
-
-(defmethod convert ((from xyz) (to srgb))
-  (declare (optimize speed))
-  (flet ((compand (value)
-           (declare ((u:f32 0f0) value))
-           (if (<= value 0.0031308f0)
-               (* value 12.92f0)
-               (- (expt (* value 1.055f0) #.(/ 2.4f0)) 0.055f0))))
-    (declare (inline compand))
-    (setf (c0 from) (compand (c0 from))
-          (c1 from) (compand (c1 from))
-          (c2 from) (compand (c2 from)))
-    (%transform-rgb-xyz from to (standard-illuminant from))))
-
 ;;; Simple sRGB to XYZ
 
 (defmethod convert ((from simple-srgb) (to xyz))
@@ -52,6 +37,26 @@
       (setf (c0 from) (invert-compansion (c0 from))
             (c1 from) (invert-compansion (c1 from))
             (c2 from) (invert-compansion (c2 from)))
-      (%transform-rgb-xyz from to standard-illuminant)
       (adapt-chromaticity to standard-illuminant)
+      (%transform-rgb-xyz from to standard-illuminant)
       to)))
+
+;;; XYZ to sRGB
+
+(defmethod convert ((from xyz) (to srgb))
+  (declare (optimize speed))
+  (flet ((compand-channel (channel)
+           (declare ((u:f32 0.0f0 1.0f0) channel))
+           (if (<= channel 0.0031308f0)
+               (* channel 12.92f0)
+               (- (* (expt channel #.(/ 2.4f0))
+                     1.055f0)
+                  0.055f0))))
+    (declare (inline compand-channel))
+    (let ((standard-illuminant (standard-illuminant to)))
+      (%transform-rgb-xyz from to standard-illuminant)
+      (adapt-chromaticity to standard-illuminant))
+    (setf (c0 to) (compand-channel (c0 to))
+          (c1 to) (compand-channel (c1 to))
+          (c2 to) (compand-channel (c2 to))))
+  to)
