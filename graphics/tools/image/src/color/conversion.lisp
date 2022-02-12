59769362b0f8ace55b2d3e9eacf32695e2072e9b
Michael Fiano <mail@mfiano.net>
2022-02-11 20:27:19 -0500

    Temporary commit to support different transfer functions.

--- graphics/tools/image/src/color/conversion.lisp
+++ graphics/tools/image/src/color/conversion.lisp
@@ -2,13 +2,13 @@
 
 (defmethod b:convert ((from rgb) (to xyz))
   (declare (optimize speed))
-  (linearize-rgb from)
+  (linearize from)
   (transform-rgb-xyz from to (illuminant-name from)))
 
 (defmethod b:convert ((from xyz) (to rgb))
   (declare (optimize speed))
   (transform-rgb-xyz from to (illuminant-name to))
-  (delinearize-rgb to))
+  (delinearize to))
 
 (defmethod b:convert ((from rgb) (to rgb))
   (declare (optimize speed))
--- graphics/tools/image/src/color/tonal-response-curves.lisp
+++ graphics/tools/image/src/color/tonal-response-curves.lisp
@@ -1,57 +1,73 @@
 (in-package #:mfiano.graphics.tools.image.color)
 
-;;; Linearize / De-linearize tonal response curves
+;;; Mixin classes
 
-(defun linearize-rgb (color)
+(defclass uniform-transfer () ())
+
+;;; Protocol
+
+(defgeneric linearize (color))
+
+(defgeneric delinearize (color))
+
+(defgeneric linearize-channel (value gamma))
+
+(defgeneric delinearize-channel (value gamma))
+
+;;; Implementation
+
+(defmethod linearize ((color uniform-transfer))
   (let ((data (data color))
         (gamma (gamma color)))
     (declare (u:f64a data))
-    (map-into data (lambda (x) (linearize-channel gamma x)) data)
+    (map-into data (lambda (x) (linearize-channel x gamma)) data)
     color))
 
-(defun delinearize-rgb (color)
+(defmethod delinearize ((color uniform-transfer))
   (let ((data (data color))
         (gamma (gamma color)))
     (declare (u:f64a data))
-    (map-into data (lambda (x) (delinearize-channel gamma x)) data)
+    (map-into data (lambda (x) (delinearize-channel x gamma)) data)
     color))
 
-;;; Linearize / Delinearize the TRC of an individual color channel.
+;; TODO: Stubs for LUV transfer functions
+;;(defmethod linearize ((color luv)))
+;;(defmethod delinearize ((color luv)))
 
-(defgeneric linearize-channel (gamma value)
-  (:method ((gamma double-float) value)
-    (expt value gamma)))
+;;; Uniform channel transfer function helpers
 
-(defgeneric delinearize-channel (gamma value)
-  (:method ((gamma double-float) value)
-    (expt value (/ gamma))))
+(defmethod linearize-channel (value (gamma double-float))
+  (expt value gamma))
 
-(defmethod linearize-channel ((gamma (eql 'L*)) value)
+(defmethod delinearize-channel (value (gamma double-float))
+  (expt value (/ gamma)))
+
+(defmethod linearize-channel (value (gamma (eql 'L*)))
   (if (<= value 0.08)
       (* 100d0 value #.(/ (float 24389/27 1d0)))
       (expt (* (+ value 0.16d0) (/ 1.16d0)) 3d0)))
 
-(defmethod delinearize-channel ((gamma (eql 'L*)) value)
+(defmethod delinearize-channel (value (gamma (eql 'L*)))
   (if (<= value #.(float 216/24389 1d0))
       (* (* value #.(float 24389/27 1d0) 100d0))
       (- (* 1.16d0 (expt value #.(float (/ 3) 1d0))) 0.16d0)))
 
-(defmethod linearize-channel ((gamma (eql 'srgb)) value)
+(defmethod linearize-channel (value (gamma (eql 'srgb)))
   (if (<= value 0.04045)
       (* value #.(/ 12.92d0))
       (expt (* (+ value 0.055d0) #.(/ 1.055d0)) 2.4d0)))
 
-(defmethod delinearize-channel ((gamma (eql 'srgb)) value)
+(defmethod delinearize-channel (value (gamma (eql 'srgb)))
   (if (<= value 0.0031308)
       (* value 12.92d0)
       (- (* (expt value #.(/ 2.4d0)) 1.055d0) 0.055d0)))
 
-(defmethod linearize-channel ((gamma (eql 'rec.709)) value)
+(defmethod linearize-channel (value (gamma (eql 'rec.709)))
   (if (< value 0.081)
       (* value #.(/ 4.5d0))
       (expt (* (+ value 0.099d0) #.(/ 1.099d0)) (/ 0.45d0))))
 
-(defmethod delinearize-channel ((gamma (eql 'rec.709)) value)
+(defmethod delinearize-channel (value (gamma (eql 'rec.709)))
   (if (< value 0.018)
       (* value 4.5d0)
       (- (* (expt value 0.45d0) 1.099d0) 0.099d0)))
