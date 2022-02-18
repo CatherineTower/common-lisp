20cff64ee8047b5af6f6397a88dc127bec436d75
Michael Fiano <mail@mfiano.net>
2022-02-18 14:35:09 -0500

    Re-organize convert.lisp

--- graphics/image/src/color/convert.lisp
+++ graphics/image/src/color/convert.lisp
@@ -1,6 +1,6 @@
 (in-package #:%mfiano.graphics.image.color)
 
-;;; RGBA -> anything (handles un-pre-multiplying alpha before calling next method)
+;;; RGB <-> *
 
 (defmethod base:convert ((from rgba) to)
   (with-pool-color (rgba 'rgba :space (space-name from) :copy from)
@@ -8,7 +8,10 @@
       (un-pre-multiply-alpha rgba))
     (call-next-method rgba to)))
 
-;;; RGB <-> XYZ
+(defmethod base:convert ((from rgb) (to rgb))
+  (rgb->* from to))
+
+(define-rgb-value-converters (rgb))
 
 (defmethod base:convert ((from rgb) (to xyz))
   (rgb->xyz from to))
@@ -18,30 +21,13 @@
 
 (define-rgb-value-converters (xyz))
 
-;;; RGB <-> xyY
-
 (defmethod base:convert ((from rgb) (to xyy))
   (rgb->* from to))
 
 (defmethod base:convert ((from xyy) (to rgb))
   (*->xyz->rgb from to))
 
-;;; RGB <-> RGB (the RGB model has multiple color spaces)
-
-(defmethod base:convert ((from rgb) (to rgb))
-  (rgb->* from to))
-
-(define-rgb-value-converters (rgb))
-
-;;; Luv <-> XYZ
-
-(defmethod base:convert ((from luv) (to xyz))
-  (luv->xyz from to))
-
-(defmethod base:convert ((from xyz) (to luv))
-  (xyz->luv from to))
-
-;;; Luv <-> RGB
+(define-rgb-value-converters (xyy))
 
 (defmethod base:convert ((from rgb) (to luv))
   (rgb->* from to))
@@ -51,7 +37,13 @@
 
 (define-rgb-value-converters (luv))
 
-;;; xyY <-> XYZ
+;;; XYZ <-> *
+
+(defmethod base:convert ((from xyz) (to luv))
+  (xyz->luv from to))
+
+(defmethod base:convert ((from luv) (to xyz))
+  (luv->xyz from to))
 
 (defmethod base:convert ((from xyz) (to xyy))
   (xyz->xyy from to))
