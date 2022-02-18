6bf121c77d413f24e2a193dea22f8f2e17285fca
Michael Fiano <mail@mfiano.net>
2022-02-18 01:12:40 -0500

    Add initargs to the ALPHA class so they're respective slots are not unbound.

--- graphics/tools/image/src/color/alpha.lisp
+++ graphics/tools/image/src/color/alpha.lisp
@@ -3,10 +3,12 @@
 (defclass alpha ()
   ((%alpha-index
     :type (integer 0 3)
-    :reader alpha-index)
+    :reader alpha-index
+    :initarg :alpha-index)
    (%pre-multiply-alpha
     :type boolean
-    :reader pre-multiply-alpha-p)))
+    :reader pre-multiplied-alpha-p
+    :initarg :pre-multiply-alpha)))
 
 (defmethod initialize-instance :around ((instance alpha) &key alpha-index pre-multiply-alpha)
   (call-next-method)
