494afa2fb4e94ef8b913a08bb63ae79a8316edae
Michael Fiano <mail@mfiano.net>
2022-02-19 20:00:26 -0500

    Tests are supposed to have ugly macros, right?

--- graphics/image/test/utils.lisp
+++ graphics/image/test/utils.lisp
@@ -3,50 +3,57 @@
 (defun color (model &optional space)
   (i:default-color model :space (or space model)))
 
-(defun compare-channels (result sequence string)
-  (let ((channels (c::channels result)))
-    (flet ((compare (expected test-func)
-             (map-into expected (lambda (x) (float x 1d0)) sequence)
-             (is channels expected string :test test-func))
-           (cmp (func)
-             (lambda (x y)
-               (funcall func x y :abs 1d-4))))
-      (etypecase channels
-        (v2:vec (compare (v2:zero) (cmp #'v2:=)))
-        (v3:vec (compare (v3:zero) (cmp #'v3:=)))
-        (v4:vec (compare (v4:zero) (cmp #'v4:=)))))))
+(defun collect-result (result sequence)
+  (let ((expected (make-array (length result) :element-type 'u:f64 :initial-element 0d0)))
+    (map-into expected (lambda (x) (float x 1d0)) sequence)
+    (cons result expected)))
 
-(defun convert (from-color to-color)
-  (i:convert from-color to-color))
-
-(defun convert/round-trip (from-color to-color)
-  (let ((to-color (convert from-color to-color))
-        (from-space (c::space-name from-color)))
-    (i:convert to-color (i:default-color (type-of from-color) :space from-space))))
-
-(defun test (from-color to-color expected)
-  (let* ((from-model/space (remove-duplicates
-                            (list (type-of from-color)
-                                  (c::space-name from-color))))
-         (to-model/space (remove-duplicates
-                          (list (type-of to-color)
-                                (c::space-name to-color))))
-         (result (convert from-color to-color))
-         (string (format nil "~{~a~^/~} -> ~{~a~^/~}"
-                         from-model/space
-                         to-model/space)))
-    (compare-channels result expected string)))
+(defun round-trip (from-color to-color)
+  (let* ((to-color (i:convert from-color to-color))
+         (from-space (c::space-name from-color))
+         (out (color (type-of from-color) from-space)))
+    (c::channels (i:convert to-color out))))
 
 (defun test/round-trip (from-color to-color expected)
-  (let* ((from-model/space (remove-duplicates
-                            (list (type-of from-color)
-                                  (c::space-name from-color))))
-         (to-model/space (remove-duplicates
-                          (list (type-of to-color)
-                                (c::space-name to-color))))
-         (result (convert/round-trip from-color to-color))
+  (let* ((from-model/space (adjoin (type-of from-color) (list (c::space-name from-color))))
+         (to-model/space (adjoin (type-of to-color) (list(c::space-name to-color))))
+         (result (round-trip from-color to-color))
          (string (format nil "~{~a~^/~} -> ~{~a~^/~} -> ~{~a~^/~}"
                          from-model/space
                          to-model/space
                          from-model/space)))
     (compare-channels result expected string)))
+
+(defmacro with-round-trips (from to &key (count 1000) (min 0d0) (max 0d0))
+  (u:once-only (min max)
+    (u:with-gensyms (from-color from-channels to-color results result fail check)
+      (destructuring-bind (from-model &optional (from-space from-model)) (u:ensure-list from)
+        (destructuring-bind (to-model &optional (to-space to-model)) (u:ensure-list to)
+          `(let ((,results nil))
+             (dotimes (#:i ,count)
+               (let* ((,from-color (color ',from-model ',from-space))
+                      (,from-channels (c::channels ,from-color))
+                      (,to-color (color ',to-model ',to-space)))
+                 (map-into ,from-channels
+                           (lambda (x)
+                             (declare (ignore x))
+                             (float (+ ,min (random (1+ (- ,max ,min)))) 1d0))
+                           ,from-channels)
+                 (let ((,result (round-trip ,from-color ,to-color)))
+                   (push (collect-result ,from-channels ,result) ,results))))
+             (let* ((,fail)
+                    (,check (every
+                             (lambda (x)
+                               (let ((pass (v3:= (car x) (cdr x))))
+                                 (unless pass
+                                   (setf ,fail x))
+                                 pass))
+                             ,results)))
+               (ok ,check
+                   (if ,fail
+                       (format nil "~a is expected to be ~a" (car ,fail) (cdr ,fail))
+                       ,(format nil "~{~a~^/~} -> ~{~a~^/~} -> ~{~a~^/~} (~d iterations)"
+                                (adjoin from-model `(,from-space))
+                                (adjoin to-model `(,to-space))
+                                (adjoin from-model `(,from-space))
+                                count))))))))))
