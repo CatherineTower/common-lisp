88be3547aef724d7d3e2c1f36c7bd1e4187be80b
Catherine Tower <catherine@towernet.dev>
2022-02-26 22:21:12 -0800

    Make TOLERANCE an optional argument to FLOAT=
    For whatever reason when I try to run a test in a dynamic context
    where *FLOAT-TOLERANCE* is LET-bound, it doesn't pick up that binding. So
    instead I pass it to the function. This is compatible with the previous
    definition, and no existing call sites have to be modified

--- graphics/image/test/utils.lisp
+++ graphics/image/test/utils.lisp
@@ -2,8 +2,8 @@
 
 (defvar *float-tolerance* 1d-10)
 
-(defun float= (x y)
-  (<= (abs (- x y)) (* *float-tolerance* (max (abs x) (abs y)))))
+(defun float= (x y &key (tolerance *float-tolerance*))
+  (<= (abs (- x y)) (* tolerance (max (abs x) (abs y)))))
 
 (defun color= (x y)
   (every (lambda (x y) (float= x y)) (c::channels x) (c::channels y)))
